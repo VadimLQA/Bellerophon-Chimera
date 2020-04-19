92631d5de6e8d9342d3eb288101606048ce9259f
Vadim Loparevich <vadim.loparevich@gmail.com>
2020-04-20 01:47:04 +0300

    recovery norwegian

---
+++ Task/norwegian/src/test/java/com/norwegian/pageobjects/manage/CheckInImpl.java
@@ -0,0 +1,12 @@
+package com.norwegian.pageobjects.manage;
+
+import com.norwegian.basepageplatform.CommonImpl;
+import org.openqa.selenium.WebDriver;
+/**
+ * @author vloparevich
+ */
+public class CheckInImpl extends CommonImpl {
+    public CheckInImpl(WebDriver driver) {
+        super(driver);
+    }
+}
