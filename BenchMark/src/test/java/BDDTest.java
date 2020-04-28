71382570a03c0faf9256d7303f3a7acc99f1e356
Vadim Loparevich <vadim.loparevich@gmail.com>
2020-04-28 23:30:43 +0300

    add bdd test class

---
+++ BenchMark/src/test/java/BDDTest.java
@@ -0,0 +1,18 @@
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+/**
+ * @author vloparevich
+ **/
+
+@CucumberOptions(features = {"features/ShoreExcursionImpl.feature"}, strict = true)
+public class BDDTest extends AbstractTestNGCucumberTests {
+    //  public static WebDriver driver;
+    /*@BeforeClass
+    public void setUP() {
+       // System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        driver = new ChromeDriver();
+     //   driver.get("https://google.com");
+    }*/
+
+}
