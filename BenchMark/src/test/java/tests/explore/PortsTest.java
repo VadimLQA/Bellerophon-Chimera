f7dca3d984edee9572fca125047cd0f9b92f225d
Vadim Loparevich <vadim.loparevich@gmail.com>
2020-05-03 17:56:23 +0300

    add dataprovider path to methods

--- BenchMark/src/test/java/tests/explore/PortsTest.java
+++ BenchMark/src/test/java/tests/explore/PortsTest.java
@@ -6,19 +6,8 @@ import com.norwegian.explore.PortsImpl;
 import com.norwegian.navigation.MainNavigationBarImpl;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-import org.yaml.snakeyaml.Yaml;
 import testdata.DataProviders;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
@@ -31,14 +20,11 @@ public class PortsTest extends UserSetUpTest {
     private MainNavigationBarImpl navMenu;
     private PortsImpl portPage;
 
-
     @BeforeClass
     public void setUp() {
         navMenu = new MainNavigationBarImpl(driver);
         navMenu.getMainBarOption(MainNavigationBarImpl.Tab.EXPLORE);
-        navMenu.getSubBarOption(MainNavigationBarImpl.SubTab.PORTS);
-
-        portPage = new PortsImpl(driver);
+        portPage = navMenu.getSubBarOption(MainNavigationBarImpl.SubTab.PORTS);
     }
 
     @Test(dataProvider = "ports-name", dataProviderClass = DataProviders.class,
@@ -54,47 +40,7 @@ public class PortsTest extends UserSetUpTest {
     }
 
 
-    private static final String YAML_DATA =
-            "nameOfThePort: Honolulu\n" +
-                    "labelOfThePort: Port Of Departure";
-
-    private static final String YAML_FILE = "src/test/java/testdata/ports.yaml";
-
-    @DataProvider(name = "yaml-data")
-    private Object[][] parseYaml() {
-        Yaml yaml = new Yaml();
-        Map<String, String> map = new HashMap();
-        map = yaml.load(YAML_DATA);
-        return new Object[][]{map.values().toArray()};
-    }
-
-    @DataProvider(name = "yaml-parser")
-    private Object[][] parseYAMLFile() throws FileNotFoundException {
-        Yaml yaml = new Yaml();
-        InputStream iStream = new FileInputStream(new File(YAML_FILE));
-        ArrayList<Map<String, String>> portsCollection = yaml.load(iStream);
-
-        String[][] ports2DArray = new String[portsCollection.size()][2];
-        int i = 0;
-        for (Map<String, String> portsMap : portsCollection) {
-            Collection<String> portsValuesCollection = portsMap.values();
-            for (Object portValue : portsValuesCollection) {
-                Map<String, String> portMap = (Map<String, String>) portValue;
-                for (Map.Entry<String, String> port : portMap.entrySet()) {
-                    if (port.getKey().equals("nameOfThePort")) {
-                        ports2DArray[i][0] = port.getValue();
-                    }
-                    if (port.getKey().equals("labelOfThePort")) {
-                        ports2DArray[i][1] = port.getValue();
-                    }
-                }
-            }
-            i++;
-        }
-        return ports2DArray;
-    }
-
-    @Test(dataProvider = "yaml-data")
+    @Test(dataProvider = "yaml-data", dataProviderClass = utils.Utility.class)
     public void testDisplayingDeparturePortYAML(String portName, String labelOfPortOfDep) {
         portPage.searchPort(portName);
         logger.info("*** verify that image is in the middle of the map ***");
@@ -103,7 +49,7 @@ public class PortsTest extends UserSetUpTest {
         assertEquals(portPage.getLabelPortOfDeparture(), labelOfPortOfDep, "Labels did not match");
     }
 
-    @Test(dataProvider = "yaml-parser")
+    @Test(dataProvider = "yaml-parser", dataProviderClass = utils.Utility.class)
     public void testDisplayingDeparturePortParserYAML(String portName, String labelOfPortOfDep) {
         portPage.searchPort(portName);
         logger.info("*** verify that image is in the middle of the map ***");
